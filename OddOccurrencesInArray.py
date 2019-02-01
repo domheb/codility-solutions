4a8958aec79fb7be194b4e06daf63d5767ea4c5e
domheb <47238611+domheb@users.noreply.github.com>
2019-02-01 10:53:17 +0100

    Lesson 2 Task 1 with comments

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -1,22 +1,20 @@
 """
 Written using Python 3.6
-Compiled on Linux Manjaro 18
 """
-
 def solution(A):
-    s1 = set()  #looking for element in set is
+
+	#Create useful utilities
+    s1 = set()  #Looking for element in set is
                 #way faster then in list
 
+	#Perform operation
     for i in range(0,len(A)):
-        if (not A[i] in s1):
+        if (not A[i] in s1): #first element of pair - add it
             s1.add(A[i])
             continue
-        if (A[i] in s1):
+        if (A[i] in s1):	#second element of pair - remove it
             s1.remove(A[i])
-
     exp_value = list(s1) #if there is odd number of elements,
                          #there will always be one left
     
     return(exp_value[0])
-    
-print(solution([9, 3, 9, 3, 9, 7, 9]))
