8979d52db6fbe590155272ec7673de63141ed522
domheb <47238611+domheb@users.noreply.github.com>
2019-02-01 12:58:59 +0100

    Add files via upload

---
+++ PermMissingElement.py
@@ -0,0 +1,38 @@
+"""
+Written using Python 3.6
+Compiled on Linux Manjaro 18
+"""
+def solution(A):
+    
+    #Check possibilities:
+    if not A: #Case 1: A is empty
+        return 1
+    
+    #Create useful variables
+    s = set(A)
+    A = list(s) #Rotating to set and back to list sorts elements
+    max = A[-1] #Get maximal value
+    length = len(A)
+    sequence_sum = 0
+    a_sum = 0
+    counter = 0
+    
+    #Check possibilities again:
+    if length < 0 or length > 100000: #Case 2: N is not in range
+        return -1
+    for i in range(0,length):
+        if not isinstance(A[i],int): #Case 3: Elements of A are not integers
+            return -1
+        if (A[i] < 1) or (A[i] > length+1): #Case 4: Elements of A are not in range
+            return -1
+    if length == 1:
+        if A[0] == 1:
+            return A[0] + 1
+    
+    #Perform operation
+    for i in range(0,length):
+        sequence_sum += i+1
+        a_sum += A[i]
+        if a_sum != sequence_sum:
+            return A[i]-1
+    return A[i]+1
