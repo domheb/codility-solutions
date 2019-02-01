de9ae9f1b9f5c843728b0ddd802ac6ff7c9ffdce
domheb <47238611+domheb@users.noreply.github.com>
2019-02-01 10:56:07 +0100

    Lesson 3 Task 1 with comments

---
+++ FrogJmp.py
@@ -0,0 +1,20 @@
+"""
+Written using Python 3.6
+Compiled on Linux Manjaro 18
+"""
+def solution(X, Y, D):
+    
+    #Check possibilities
+    if ((X < 1) or (Y < 1) or (D < 1) or
+    (X > 1000000000) or (Y > 1000000000) or
+    (D > 1000000000)):
+        return -1
+    if (X > Y): #Case 2: X is bigger then Y
+        return -1
+    
+    #Perform operation
+    jumps = (Y - X) // D
+    if ((Y - X) % D): #if there is a rest, it means +1 jump
+        jumps+=1
+    
+    return jumps
