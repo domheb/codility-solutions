7e06b101efa7294f98e70766ee16b946ebf8409f
domheb <47238611+domheb@users.noreply.github.com>
2019-02-01 10:47:23 +0100

    Update comments

--- BinaryGap.py
+++ BinaryGap.py
@@ -2,15 +2,16 @@
 Written using Python 3.6
 Compiled on Linux Manjaro 18
 """
-
 def solution(N):
+
+    #Create useful variables
 	bin_number = []
 
-	if not isinstance(N, int): #check if N is an integer
+    #Check possibilities:
+	if not isinstance(N, int): #Case 1: N is not an intiger
 		return(-1)
-
-	if N == 1 or N == 0: #check if N is equal to 0 or 1
-		return(0)		#if yes, then there is no gap
+	if N == 1 or N == 0: #Case 2: N is equal to 0 or 1 so no gap
+		return(0)		
 	else:
 		bin_number.append(1) #bin_number always starts with 1
 		reverse_bin_number = [] #all the next 0 and 1
@@ -19,16 +20,16 @@ def solution(N):
 			reverse_bin_number.append(temp)
 			N = N // 2
 
+	#Perform reversing
 	length = len(reverse_bin_number)
-
 	for i in range(length-1,-1,-1):
 		bin_number.append(reverse_bin_number[i]) #here they are reversed
-
 	length += 1 #bin_number has 1 more element -> the first one
+
+	#Perform main operation
 	binary_gap = 0
 	binary_gap_max = 0
 	is_counting = 0
-
 	for i in range(0,length):
 		#1) start counting
 		if is_counting == 0 and bin_number[i] == 1:
@@ -40,11 +41,9 @@ def solution(N):
 			continue
 		#3) stop counting
 		if is_counting == 1 and bin_number[i] == 1:
-			if binary_gap > binary_gap_max:
+			if binary_gap > binary_gap_max: #maximize binary_gap_max if possible
 				binary_gap_max = binary_gap
 			binary_gap = 0
 			continue
 
 	return(binary_gap_max)
-
-print(solution(805306373))
