f747327494a8bd67c45798773bde1b5195d31298
domheb <47238611+domheb@users.noreply.github.com>
2019-02-07 12:57:59 +0100

    Update and rename MissingInteger.py to Lesson 4 - Counting Elements/Python/MissingInteger.py

--- Lesson 4 - Counting Elements/Python/MissingInteger.py
+++ Lesson 4 - Counting Elements/Python/MissingInteger.py
@@ -11,20 +11,20 @@ def solution(A):
     
     #Check possibilities:
     neg_L = [A[i] for i in range(0,length) if A[i] < 0]
-    if len(neg_L) == length: #Case 1: All elements are negative
+    if len(neg_L) == length: #All elements are negative
         return(1)
     A = list(s - set(neg_L))  #Removing negative values
-    if not (A): #Case 2: N == 0, so returns error
+    if not (A): #N == 0, so returns error
         return -1
-    if length == 1: #Case 3: 1 element only
+    if length == 1: #1 element only
         if A[0] == 1:
-            return 2 #Case 3.1: element == 1
+            return 2 #element == 1
         else:
-            return 1 #Case 3.2: element > 1
+            return 1 #element > 1
             
-    #Perform standard operation
+    #Perform operation
     normal_series = [i for i in range(1,length+1)]
     if sum(A) == sum(normal_series): 
-        return max(A)+1 #Case 4.1: list is complete
+        return max(A)+1 #list is complete
     else:
-        return list(set(normal_series) - s)[0] #Case 4.2: list is incomplete
+        return list(set(normal_series) - s)[0] #list is incomplete
